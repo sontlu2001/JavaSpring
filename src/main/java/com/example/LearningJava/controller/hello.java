dca21bfe5cd8ef840f76ccf1621889583d71be0f
sontlu2001 <sonnvc2001@gmail.com>
2025-03-25 11:20:26 +0700

    2.Config project with file properties  and file yaml

--- src/main/java/com/example/LearningJava/controller/hello.java
+++ src/main/java/com/example/LearningJava/controller/hello.java
@@ -1,12 +1,19 @@
 package com.example.LearningJava.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class hello {
+
+    @Value("${player.name}")
+    private String Name;
+    @Value("${player.age}")
+    private int Age;
+
     @GetMapping("v1/api/hello")
     public String status(){
-        return "Hello";
+        return Name + " "+ Age;
     }
 }
